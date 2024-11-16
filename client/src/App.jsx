3e598e1b5577dbfee1d1763474435ef67122968b
Sejan <sejansiju2020@gmail.com>
2024-11-16 13:23:03 +0600

    jhjjhghgfgffdfkkkkj

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,76 +1,93 @@
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, Navigate } from "react-router-dom";
 
-import AuthLogin from "./pages/auth/login"
-import AuthRegister from "./pages/auth/register"
-import AuthLayout from "./components/auth/layout"
-import AdminLayout from "./components/admin-view/layout"
-import AdminDashboard from "./pages/admin-view/dashboard"
+import AuthLogin from "./pages/auth/login";
+import AuthRegister from "./pages/auth/register";
+import AuthLayout from "./components/auth/layout";
+import AdminLayout from "./components/admin-view/layout";
+import AdminDashboard from "./pages/admin-view/dashboard";
 
-import ShoppingViewLayout from "./components/shopping-view/layout"
-import NotFound from "./components/shopping-view/notfound"
-import ShoppingHome from "./pages/shopping-view/home"
-import ShoppingListing from "./pages/shopping-view/listing"
-import ShoppingAccount from "./pages/shopping-view/account"
-import CheckAuth from "./components/common/check-auth"
-import { useDispatch, useSelector } from "react-redux"
-import { useEffect } from "react"
-import { checkAuth } from "./store/auth-slice"
-import AdminProducts from "./pages/admin-view/products"
-import AdminOrders from "./pages/admin-view/orders"
-import Footer from "./pages/shopping-view/footer"
-
-
-
+import ShoppingViewLayout from "./components/shopping-view/layout";
+import NotFound from "./components/shopping-view/notfound";
+import ShoppingHome from "./pages/shopping-view/home";
+import ShoppingListing from "./pages/shopping-view/listing";
+import ShoppingAccount from "./pages/shopping-view/account";
+import CheckAuth from "./components/common/check-auth";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { checkAuth } from "./store/auth-slice";
+import AdminProducts from "./pages/admin-view/products";
+import AdminOrders from "./pages/admin-view/orders";
 
 function App() {
-
-
-  const { isAuthenticated, user, isLoading } = useSelector((state) =>
-    state.auth
-  );
+  const { isAuthenticated, user, isLoading } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
+
   useEffect(() => {
-    const token = JSON.parse(sessionStorage.getItem('token'))
+    const token = JSON.parse(sessionStorage.getItem("token"));
     dispatch(checkAuth(token));
   }, [dispatch]);
-  if (isLoading) return <div>Loading</div>
+
+  if (isLoading) return <div>Loading...</div>;
 
   return (
     <div className="flex flex-col overflow-hidden bg-white">
-
       <Routes>
-        <Route path="/auth" element={
-          <CheckAuth isAuthenticated={isAuthenticated} user={user}>
-            <AuthLayout />
-          </CheckAuth>
-        } >
+        {/* Root route to handle base URL */}
+        <Route
+          path="/"
+          element={
+            isAuthenticated ? (
+              <Navigate to="/shop/home" replace />
+            ) : (
+              <Navigate to="/auth/login" replace />
+            )
+          }
+        />
 
+        {/* Authentication Routes */}
+        <Route
+          path="/auth"
+          element={
+            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+              <AuthLayout />
+            </CheckAuth>
+          }
+        >
           <Route path="login" element={<AuthLogin />} />
           <Route path="register" element={<AuthRegister />} />
         </Route>
-        <Route path="/admin" element={
-          <CheckAuth isAuthenticated={isAuthenticated} user={user}>
-            <AdminLayout />
-          </CheckAuth>
-        } >
+
+        {/* Admin Routes */}
+        <Route
+          path="/admin"
+          element={
+            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+              <AdminLayout />
+            </CheckAuth>
+          }
+        >
           <Route path="dashboard" element={<AdminDashboard />} />
           <Route path="products" element={<AdminProducts />} />
           <Route path="orders" element={<AdminOrders />} />
         </Route>
-        <Route path="/shop" element={
-          <CheckAuth isAuthenticated={isAuthenticated} user={user}>
-            <ShoppingViewLayout />
-          </CheckAuth>
 
-        } >
+        {/* Shopping Routes */}
+        <Route
+          path="/shop"
+          element={
+            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+              <ShoppingViewLayout />
+            </CheckAuth>
+          }
+        >
           <Route path="*" element={<NotFound />} />
           <Route path="home" element={<ShoppingHome />} />
           <Route path="listing" element={<ShoppingListing />} />
           <Route path="account" element={<ShoppingAccount />} />
         </Route>
       </Routes>
-    </div >
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
