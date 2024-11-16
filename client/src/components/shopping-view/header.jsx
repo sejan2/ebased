8603e9fb3dbb3ba342a23d8d35cd13e64215e624
Sejan <sejansiju2020@gmail.com>
2024-11-16 06:49:22 +0600

    sghfhgdsmoo

--- client/src/components/shopping-view/header.jsx
+++ client/src/components/shopping-view/header.jsx
@@ -37,7 +37,7 @@ function HeaderRightContent() {
         // dispatch(logoutUser())
         dispatch(resetTokenAndCredentials())
         sessionStorage.clear();
-        navigate("auth/login")
+        navigate("/auth/login")
     }
 
     useEffect(() => {
