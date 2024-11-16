6b3d0967171938b59ec9840eba48d0a1bdc4259f
Sejan <sejansiju2020@gmail.com>
2024-11-16 06:10:11 +0600

    sghfhghgjh

--- client/src/components/admin-view/header.jsx
+++ client/src/components/admin-view/header.jsx
@@ -1,7 +1,7 @@
 import { AlignJustify, LogOut } from "lucide-react";
 import { Button } from "../ui/button";
 import { useDispatch } from "react-redux";
-import { logoutUser } from "@/store/auth-slice";
+import { logoutUser, resetTokenAndCredentials } from "@/store/auth-slice";
 import { useNavigate } from "react-router-dom";
 
 function AdminHeader({ setOpen }) {
