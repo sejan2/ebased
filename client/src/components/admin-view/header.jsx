5b6602934e40c797b5e76f539b9d09eb2a544582
Sejan <sejansiju2020@gmail.com>
2024-11-16 06:46:42 +0600

    sghfhgdsmk

--- client/src/components/admin-view/header.jsx
+++ client/src/components/admin-view/header.jsx
@@ -12,7 +12,7 @@ function AdminHeader({ setOpen }) {
         // dispatch(logoutUser());
         dispatch(resetTokenAndCredentials())
         sessionStorage.clear();
-        navigate("/auth/login")
+        navigate("auth/login")
     }
 
     return (
